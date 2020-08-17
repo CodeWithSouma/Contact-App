07b33c05a06a27ea9fbeb65f6cacd74ab50c0936
Souma <souma.hitech@gmail.com>
2020-08-18 02:10:28 +0530

    refator code and use map function.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,25 @@ import Card from "./Card"
 import Heading from "./Heading";
 import contacts from "../contacts";
 
+function createCard(contact) {
+  return (
+    <Card
+        key ={contact.id}
+        name ={contact.name}
+        src={contact.imgURL}
+        tel={contact.phone}
+        email={contact.email}
+      />
+  );
+}
+
 
 function App() {
   return (
     <div>
       <Heading/>
-        <Card
+      {contacts.map(createCard)}
+        {/* <Card
         name ={contacts[0].name}
         src={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -25,7 +38,7 @@ function App() {
         src={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function Avatar(props) {
     return(<img className="circle-img"
-                src={props.src}/>);
+                src={props.src}
+                alt="not found!"
+                />);
 }
 
 export default Avatar;
