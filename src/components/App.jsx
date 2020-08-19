3294b5e6f784dc880515b0fd834798df94116a6b
Souma <souma.hitech@gmail.com>
2020-08-19 08:42:24 +0530

    refactore code.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,25 +20,7 @@ function App() {
   return (
     <div>
       <Heading/>
-      {contacts.map(createCard)}
-        {/* <Card
-        name ={contacts[0].name}
-        src={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name ={contacts[1].name}
-        src={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name ={contacts[2].name}
-        src={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      /> */}
+      {contacts.map(createCard)}//it work like forEach loop 
     </div>
   );
 }
